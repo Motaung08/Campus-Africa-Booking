954d6b694fbefb97b20c1f516e2167013e3f923f
phantom820 <1611821@students.wits.ac.za>
2020-09-01 18:33:32 +0200

    added booking/track booking display hide and show

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,10 +1,29 @@
 
 mainApp.controller( "mainController", function( $scope , $http ) {
-		
+			
+			// action variaable booking/ tracking
+			$scope.mapAction = function(action){
+
+				if(action==="booking"){
+					$scope.showBookingMenu=true;
+					$scope.showBookingTracking=false;
+					$scope.actionHeading="Bookings"
+				}
+
+				else if(action==="track booking"){
+					$scope.showBookingMenu=false;
+					$scope.showBookingTracking=true;
+					$scope.actionHeading="Track booking"
+				}
+			}
+
 			//regular expression to check for SA id number pattern
 			$scope.regex='(([0-9][0-9][0-1][0-9][0-3][0-9])([0-9][0-9][0-9][0-9])([0-1])([0-9])([0-9]))'
 			
 			//building variables array
 			$scope.buildings=["49 Jorissen"]
+
+			//apartment types
+			$scope.roomTypes=["2 Sharing Apartment","3 Sharing Apartment"];
 
 			});
